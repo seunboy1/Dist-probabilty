9e8f796841741201840c1ba88a6ded1bb08c61df
Adeyo Oluwaseun <oadeyo@seamfix.com>
2020-07-18 17:00:23 +0100

    init commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Dis_Probability',
-      version='0.1',
+      version='0.2',
       description='This is a fundamental package that helps compute both binomial and Gaussain probability',
       packages=['Dist_Probability'],
       author =  "Oluwaseun Adeyo",
